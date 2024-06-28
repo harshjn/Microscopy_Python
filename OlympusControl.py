cd6f218832816af8229e4ea8d99cbce63bed0d82
Harsh Jain <harshjainldh@gmail.com>
2024-06-28 13:59:52 +0530

    Create OlympusControl.py

---
+++ OlympusControl.py
@@ -0,0 +1,47 @@
+# After starting micro-manager and loading the Hardware configuration file, control it with python
+from pycromanager import Core
+
+# Initialize the core
+core = Core()
+
+# List all loaded devices
+devices = core.get_loaded_devices()
+
+# Convert mmcorej.StrVector to a Python list
+device_list = [devices.get(i) for i in range(devices.size())]
+
+# Print the list of devices
+print("Connected devices:")
+for device in device_list:
+    print(device)
+
+#%% Get com port for any device. In this case, Olympus Hub.
+
+try:
+    # Attempt to get the COM port property
+    com_port = core.get_property('OlympusHub', "Port")
+    print(f"  COM Port: {com_port}")
+except:
+    # Handle the case where the device does not have a "Port" property
+    print(f"  COM Port: Not applicable")
+
+#%% Set the shutter on or off
+core.set_property('Shutter1', 'State', '1')
+core.set_property('Shutter1', 'State', '0')
+
+# Set the z-position
+new_focus_position = 3000  # Specify the desired focus position
+core.set_property('ManualFocus', 'Position', str(new_focus_position))
+print(f"Moved Z focus to position: {new_focus_position}")
+
+#%%
+# Example: Turn on the TransmittedLamp
+core.set_property('TransmittedLamp', 'State', '1')
+print("Transmitted lamp is turned on.")
+
+# Example: Turn off the TransmittedLamp
+core.set_property('TransmittedLamp', 'State', '0')
+print("Transmitted lamp is turned off.")
+
+# Example: Disconnect from the OlympusHub
+core.unload_device('OlympusHub')
