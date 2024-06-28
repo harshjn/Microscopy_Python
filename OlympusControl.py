8d1929408b44dc308cbd70dc911385860fe4025e
Harsh Jain <harshjainldh@gmail.com>
2024-06-28 14:05:07 +0530

    Update OlympusControl.py

--- OlympusControl.py
+++ OlympusControl.py
@@ -29,6 +29,15 @@ except:
 core.set_property('Shutter1', 'State', '1')
 core.set_property('Shutter1', 'State', '0')
 
+
+# Retrieve the far limit of the manual focus
+far_limit = core.get_property('ManualFocus', 'FarLimit')
+print(f"Far limit of the manual focus: {far_limit}")
+
+# Move manual focus to the far point
+core.set_property('ManualFocus', 'Position', far_limit)
+print(f"Moved manual focus to the far point: {far_limit}")
+
 # Set the z-position
 new_focus_position = 3000  # Specify the desired focus position
 core.set_property('ManualFocus', 'Position', str(new_focus_position))
